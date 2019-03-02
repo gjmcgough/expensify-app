199404bc06ddef9592503cd2b1986c50133bd8f2
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-01 16:37:43 -0800

    break components out into individual files,set up routing and import/exports

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+  <div>
+    CREATE EXPENSE
+  </div>
+);
+
+export default AddExpensePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>NavLink
+    EDIT EXPENSE
+  </div>
+);
+
+export default EditExpensePage;
