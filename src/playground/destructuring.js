111eabe8cf59b6b9feadddc70c849e9df06f584a
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-06 15:04:56 -0800

    es6 destructuring excercise 1

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,35 @@
-console.log("TESSSST");
+// const person = {
+//   name: "Greg",
+//   age: 30,
+//   location: {
+//     city: 'San Diego',
+//     temp: 65
+//   }
+// };
+//
+// //renaming 'name' as 'firstName' and setting default value for firstName
+// const {name: firstName = 'Anonymous', age} = person;
+// console.log(`${firstName} is ${age}.`);
+// // above is the same as below
+// // const name = person.name;
+// // const age = person.age;
+//
+// //below 'temp' is being renamed as 'temperature'
+// const {city, temp: temperature} = person.location;
+// if (city && temperature){
+//   console.log(`Its ${temperature} in ${city}.`);
+// }
+
+const book = {
+  title: 'Ego',
+  author: 'Ryan Holliday',
+  publisher: {
+    name:'penguin'
+  }
+}
+
+const {name: publisherName = 'self-published'} = book.publisher;
+
+
+
+console.log(publisherName);
