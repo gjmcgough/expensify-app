2e20023f57cfb5c0ff0dcdcbcb33a5bd66803504
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-12 11:42:11 -0700

    added text filter input, and remove expense button. both working

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default (props) => (
+ const ExpenseListItem = (props) => (
   <div>
     <h2>Expense: {props.description}</h2>
     <p>Amount: ${props.amount} - Created at: {props.createdAt}</p>
+    <button onClick={() => {
+        props.dispatch(removeExpense(props.id));
+      }}
+    >Remove</button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
