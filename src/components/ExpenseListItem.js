10f804ae0d28ec95a64073ccdcb89e59aafaac92
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-18 16:15:08 -0700

    create dynamic link for title of items to edit expense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
  const ExpenseListItem = (props) => (
   <div>
-    <h2>Expense: {props.description}</h2>
+    <Link to={`/edit/${props.id}`}>
+      <h2>Expense: {props.description}</h2>
+    </Link>
     <p>Amount: ${props.amount} - Created at: {props.createdAt}</p>
     <button onClick={() => {
         props.dispatch(removeExpense(props.id));
