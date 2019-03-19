97f13be63e43a728f57a32dd89ead7f14a218215
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-18 17:06:03 -0700

    wire up edit expense page, now able to edit expenses and then redirect to dashboard

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,12 +4,16 @@ import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment()
-  };
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      description: props.expense ? props.expense.description : '',
+      note: props.expense ? props.expense.note : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment()
+    };
+  }
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
