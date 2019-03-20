707cdcf7ab763d223682bdfdf064279c39d8da71
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-20 16:55:11 -0700

    add react shallow renderer dependency. build snapshot test for header menu, 28 tests passing total

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink><br/><br/>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink><br/><br/>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink><br/><br/>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
   </header>
 );
 
