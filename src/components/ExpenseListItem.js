5dd5271a806a8153e59a3f0a0445c5dd4dd8adfe
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-26 17:19:48 -0700

    create test snapshot form ExpenseListItem. 31 passing tests

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,13 @@ import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
 
- const ExpenseListItem = (props) => (
+ export const ExpenseListItem = (props) => (
   <div>
     <Link to={`/edit/${props.id}`}>
       <h2>Expense: {props.description}</h2>
     </Link>
     <p>Amount: ${props.amount} - Created at: {props.createdAt}</p>
-  
+
   </div>
 );
 
