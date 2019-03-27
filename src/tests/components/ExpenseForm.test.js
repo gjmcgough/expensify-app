172581ea9fc4814f3ee1b85c49801cafe36ce8e8
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-27 15:10:58 -0700

    add test for invalid form submittal and description value

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,23 @@ test('should render ExpenseForm with data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]}/>);
   expect(wrapper).toMatchSnapshot();
 });
+
+
+test('should render an error if form is submitted without a description or amount', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('shoul set description on input change', () => {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: { value }
+  });
+  expect(wrapper.state('description')).toBe(value);
+});
