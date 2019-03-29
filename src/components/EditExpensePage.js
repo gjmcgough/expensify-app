a1611c7e34c6285142ca04f34eacba472ad71508
greg.mcgough <greg.mcgough@servicenow.com>
2019-03-29 14:30:18 -0700

    break out EditExpensePage into class based component successfully

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,25 +3,29 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
-          props.history.push('/');
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense(props.expense.id));
-          props.history.push('/');
-        }}
-      >Remove</button>
-    </div>
-  );
+export class EditExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  }
+  onRemove = () => {
+    this.props.removeExpense(this.props.expense.id);
+    this.props.history.push('/');
+  }
+  render() {
+    console.log("PROPS: " + this.props.expense);
+    return (
+      <div>
+        <ExpenseForm
+          expense={this.props.expense}
+          onSubmit={this.onSubmit}
+        />
+        <button
+          onClick={this.onRemove}
+        >Remove</button>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, props) => {
@@ -30,4 +34,9 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapDispatchToProps = (dispatch) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: (data) => dispatch(removeExpense(data))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
