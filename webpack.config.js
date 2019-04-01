a87f49650bcc208aaf1b6368bccf17c95e7b2d66
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-01 16:35:14 -0700

    update webpack to user sourcemaps in development environment. also extracting CSS files and loading them externally

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // console.log(path.join(__dirname, 'public'));
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
@@ -18,14 +20,27 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },{
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-sourcemap',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
