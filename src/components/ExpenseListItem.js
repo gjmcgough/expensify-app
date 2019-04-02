4e30bfa003b04f44a6a590abe08769f738cc0d1a
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-02 16:47:55 -0700

    install numeraljs, update expenselistitem to show corrct dates and amounts formats

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
  export const ExpenseListItem = (props) => (
   <div>
     <Link to={`/edit/${props.id}`}>
       <h2>Expense: {props.description}</h2>
     </Link>
-    <p>Amount: ${props.amount} - Created at: {props.createdAt}</p>
-
+    <p>
+      Amount: ${numeral(props.amount / 100).format('$0,0.00')}
+      -
+      Created at: {moment(props.createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
