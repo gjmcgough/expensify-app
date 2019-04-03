c221b34375ea1a144238be351812b120b4ef75f3
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-03 15:51:09 -0700

    refactor to use expenses fixture for total selector test

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -1,29 +1,17 @@
 import selectExpensesTotal from '../../selectors/expenses-total';
-
-
+import expenses from '../fixtures/expenses';
 
 test('should return 0 if no expenses', () => {
-  const expenses = [];
-  const result = selectExpensesTotal(expenses);
+  const result = selectExpensesTotal([]);
   expect(result).toEqual(0);
 });
 
 test('should correctly add up a single expense', () => {
-  const expenses = [{
-    amount: 100
-  }];
-  const result = selectExpensesTotal(expenses);
-  expect(result).toEqual(100);
+  const result = selectExpensesTotal([expenses[0]]);
+  expect(result).toEqual(195);
 });
 
 test('should correctly add up multiple expenses', () => {
-  const expenses = [{
-      amount: 10
-    },{
-      amount: -10
-    },{
-      amount: 500
-  }];
   const result = selectExpensesTotal(expenses);
-  expect(result).toEqual(500);
+  expect(result).toEqual(114195);
 });
