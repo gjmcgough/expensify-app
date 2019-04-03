fac2c9e7657252982e8315b7d5d474fb485b393b
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-03 16:05:40 -0700

    refactor total expenses selector

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,14 +1,9 @@
 export default (expenses) => {
-  const amountsArray = [];
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  let total = 0;
   if (expenses.length <= 0) {
-    return total
+    return 0;
   } else {
-    expenses.map((expense) => {
-      amountsArray.push(expense.amount);
-    });
-    total = amountsArray.reduce(reducer);
-    return total;
+    const expensesArray = expenses.map((expense) => expense.amount);
+    return expensesArray.reduce(reducer);
   };
 };
