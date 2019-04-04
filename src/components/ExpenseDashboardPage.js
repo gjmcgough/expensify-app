897b6ccb9232d5b7d967f4a962bdd8aec252ce69
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-03 17:39:00 -0700

    Move the rendering of the ExpenseSummary to the header component. Everything is working

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-import ExpensesSummary from './ExpensesSummary';
-import selectExpenses from '../selectors/expenses'
-import selectExpensesTotal from '../selectors/expenses-total';
 
-export class ExpenseDashboardPage extends React.Component {
-  expensesCount = (expenses) => {
-    return this.props.expenses.length;
-  };
-  expensesTotal = (expenses) => {
-    return selectExpensesTotal(this.props.expenses);
-  };
-  render() {
-    return (
-      <div>
-        <ExpensesSummary
-          expensesCount={this.expensesCount}
-          expensesTotal={this.expensesTotal}
-          expenses={this.props.expenses}
-        />
-        <ExpenseListFilters />
-        <ExpenseList />
-      </div>
-    );
-  }
-}
+const ExpenseDashboardPage = () => (
+<div>
+<ExpenseListFilters />
+<ExpenseList />
+</div>
+);
 
-const mapStateToProps = (state) => {
-  return {
-    expenses: selectExpenses(state.expenses, state.filters)
-  };
-};
-
-export default connect(mapStateToProps)(ExpenseDashboardPage);
+export default ExpenseDashboardPage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ExpensesSummary from './ExpensesSummary';
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTotal from '../selectors/expenses-total';
 
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-  </header>
-);
+export class Header extends React.Component {
+  expensesCount = (expenses) => {
+    return this.props.expenses.length;
+  };
+  expensesTotal = (expenses) => {
+    return selectExpensesTotal(this.props.expenses);
+  };
+  render() {
+    return (
+      <div>
+        <ExpensesSummary
+          expensesCount={this.expensesCount}
+          expensesTotal={this.expensesTotal}
+          expenses={this.props.expenses}
+        />
+        <header>
+          <h1>Expensify</h1>
+          <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+          <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        </header>
+      </div>
+    );
+  };
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
+};
+
+export default connect(mapStateToProps)(Header);
