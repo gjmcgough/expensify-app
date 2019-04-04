3b94add534f7206220b1b44dff8585297e7cc936
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-03 17:57:51 -0700

    ExpensesSummary working. Needs to be tested correctly, the passing tests are not good tests

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ExpensesSummary } from '../../components/ExpensesSummary';
+import ExpensesSummary from '../../components/ExpensesSummary';
 import expenses from '../fixtures/expenses';
 
+let expensesCount, expensesTotal, wrapper;
+
+beforeEach(() => {
+  expensesCount = jest.fn();
+  expensesTotal = jest.fn();
+  wrapper = shallow(
+    <ExpensesSummary
+      expenses={expenses}
+      expensesCount={expensesCount}
+      expensesTotal={expensesTotal}
+    />);
+});
+
 test('should render single expense singularily' , () => {
-  const wrapper = shallow(<ExpensesSummary />);
+
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should render multiple expenses with plural expenses' , () => {
-  const wrapper = shallow(<ExpensesSummary />);
   expect(wrapper).toMatchSnapshot();
 });
