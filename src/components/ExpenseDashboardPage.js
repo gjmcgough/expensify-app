ebdf166e425e6cce1d9211a9dc3b44259a208a49
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-04 15:32:05 -0700

    revert DashboardPage to SFC as no class needed

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -6,21 +6,20 @@ import ExpensesSummary from './ExpensesSummary';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses'
 
-export class ExpenseDashboardPage extends React.Component {
-  render() {
-    return (
+export const ExpenseDashboardPage = (props) => {
+  return (
     <div>
-      {console.log("PPPROPS! " + this.props.expenses)}
+      {console.log("PPPROPS! " + props.expenses)}
       <ExpensesSummary
-        expensesCount={this.props.expenses.length}
-        expensesTotal={selectExpensesTotal(this.props.expenses)}
+        expensesCount={props.expenses.length}
+        expensesTotal={selectExpensesTotal(props.expenses)}
       />
       <ExpenseListFilters />
       <ExpenseList />
     </div>
-    );
-  };
+  );
 };
+
 
 const mapStateToProps = (state) => {
   return {
