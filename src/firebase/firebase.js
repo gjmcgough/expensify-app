ba58e0a6967be6466e00b6b118b3952ec2ecac12
greg.mcgough <greg.mcgough@servicenow.com>
2019-04-25 16:01:51 -0700

    firebase subscription practice fetching, setting, displaying data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,22 +14,86 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: "Greg M",
-  age: 31,
-  isSingle: false,
-  location: {
-    country: 'United States',
-    state: 'California',
-    city: 'San Diego'
-  }
-}).then(() => {
-  console.log('DATA IS SAVED!');
-}).catch((e) => {
-  console.log('ThIs fAiLeD', e);
-});
+const onValueChange = (snapshot) => {
+  const response = snapshot.val();
+  console.log(response);
+  console.log(`${response.name} is a ${response.job.title} at ${response.job.company}`);
+};
 
-const singleRef = database.ref('isSingle');
+database.ref().on('value', onValueChange),
+  (e) => {
+    console.log('Error fetching data', e);
+  };
+
+setTimeout(() => {
+  database.ref().update({
+    name: 'GREGORIO',
+    'job/company': 'ServiceNow',
+    'job/title': 'Associate Developer'
+  });
+}, 5000);
+
+// console.log(database.ref(name) + ' is a ' + database.ref(job) + ' at '+ database.ref(company));
+
+// const onValueChange = (snapshot) => {
+//   console.log(snapshot.val());
+// };
+//
+// database.ref().on('value', onValueChange), (e) => {
+//   console.log('error fetching data', e);
+// };
+//
+// setTimeout(() => {
+//   database.ref('age').set(55);
+// }, 3500);
+//
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 7000);
+//
+// setTimeout(() => {
+//   database.ref('age').set(100);
+// }, 10500);
+
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+// }).catch((e)=>{
+//   console.log('Error fetching data', e);
+// })
+
+//
+// database.ref().set({
+//   name: "Greg M",
+//   stressLevel: 6,
+//   age: 31,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     country: 'United States',
+//     state: 'California',
+//     city: 'San Diego'
+//   }
+// }).then(() => {
+//   console.log('DATA IS SAVED!');
+// }).catch((e) => {
+//   console.log('ThIs fAiLeD', e);
+// });
+//
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+//
+// const singleRef = database.ref('isSingle');
+
+
 
 //
 // singleRef.remove().then(() => {
