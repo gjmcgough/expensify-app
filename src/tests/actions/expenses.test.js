985e19ece5ad37d985788f62dd7c2e0b6e8bb32d
greg.mcgough <greg.mcgough@servicenow.com>
2019-05-22 16:52:06 -0700

    updating test case assertions to test for async redux actions

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,9 @@
-import { addExpense, removeExpense, editExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, removeExpense, editExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should set up remove expense action object', () => {
   const result = removeExpense('123abc');
@@ -27,6 +31,24 @@ test('should set up add expense object with input values', () => {
     type: 'ADD_EXPENSE',
     expense: expenses[2]
   });
+});
+
+test('should add expense to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: 'Mouse',
+    note: 'Eek a',
+    amount: 12345,
+    createdAt: 10000
+  };
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(2).toBe(2);
+    done();
+  })
+});
+
+test('should add expense with defaults to database and store', () => {
+  
 });
 
 // test('should set up add expense action object with DEFAULT values', () => {
