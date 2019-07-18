f69ed7a6e476658c80192f1553ff4f2e744328c6
greg.mcgough <greg.mcgough@servicenow.com>
2019-07-18 11:10:03 -0700

    set up test DB and environment variables in webpack

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyBDAjby21zb-SVNwvRpg1upduiA11zsCw8",
-  authDomain: "expensify-wu.firebaseapp.com",
-  databaseURL: "https://expensify-wu.firebaseio.com",
-  projectId: "expensify-wu",
-  storageBucket: "expensify-wu.appspot.com",
-  messagingSenderId: "485874070687"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
